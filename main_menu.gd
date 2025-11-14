393ac233e8b777e7e993b7958551aba1a7a2e73c
Severian0 <darkdalek95@gmail.com>
2025-11-14 23:30:51 +0000

    Added connection to game.tcsn from menu

--- main_menu.gd
+++ main_menu.gd
@@ -12,7 +12,7 @@ func _process(delta: float) -> void:
 
 
 func _on_start_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://scenes/game.tscn")
 
 
 func _on_options_pressed() -> void:
